c97ccf1547bbc578469aff9b0cded8bcb25d5c1a
Zetao <zetao_wu@brown.edu>
2024-09-07 13:24:36 -0400

    implement the getAllUsers api

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,13 @@
-export const getAllUsers = () => {
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
     //get all users from db
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "Ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error", cause: error });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,18 @@
-export const getAllUsers = () => {
-    //get all users from db
-}
+import { Request, Response, NextFunction } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //get all users from db
+  try {
+    const users = await User.find();
+    return res.status(200).json({message: "Ok", users});
+  } catch (error) {
+    console.log(error)
+    return res.status(200).json({message: "Error", cause: error});
+
+  }
+};
