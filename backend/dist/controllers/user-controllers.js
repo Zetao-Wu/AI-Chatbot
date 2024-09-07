7f32cf1d70b0322ba9c6200d0b845aa6d3b7835f
Zetao <zetao_wu@brown.edu>
2024-09-07 13:54:39 -0400

    implementing login validator and login api req

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import { hash } from 'bcrypt';
+import { hash, compare } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     //get all users from db
     try {
@@ -15,11 +15,35 @@ export const userSignup = async (req, res, next) => {
     //user signup
     try {
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User Already Registered");
         const hashedPassword = await hash(password, 10);
         const user = new User({
-            name, email, password: hashedPassword
+            name,
+            email,
+            password: hashedPassword,
         });
         await user.save();
+        return res.status(201).json({ message: "Ok", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error", cause: error });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    //user login
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Invalid Credentials");
+        }
         return res.status(201).json({ message: "Ok", id: user._id.toString() });
     }
     catch (error) {
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
-import {hash} from 'bcrypt'
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -10,30 +10,58 @@ export const getAllUsers = async (
   //get all users from db
   try {
     const users = await User.find();
-    return res.status(200).json({message: "Ok", users});
+    return res.status(200).json({ message: "Ok", users });
   } catch (error) {
-    console.log(error)
-    return res.status(200).json({message: "Error", cause: error});
-
+    console.log(error);
+    return res.status(200).json({ message: "Error", cause: error });
   }
 };
 
 export const userSignup = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    //user signup
-    try {
-    const {name, email, password} = req.body;
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //user signup
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User Already Registered");
     const hashedPassword = await hash(password, 10);
-      const user = new User({
-        name, email, password: hashedPassword
-      })
-      await user.save();
-      return res.status(201).json({message: "Ok", id:user._id.toString()});
-    } catch (error) {
-      console.log(error)
-      return res.status(200).json({message: "Error", cause: error});
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    return res.status(201).json({ message: "Ok", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "Error", cause: error });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //user login
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send("User not registered");
     }
-  };
+
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Invalid Credentials");
+    }
+
+    return res.status(201).json({ message: "Ok", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "Error", cause: error });
+  }
+};
