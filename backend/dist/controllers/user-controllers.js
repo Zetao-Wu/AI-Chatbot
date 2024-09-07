c57b07fb75ed50964e2cd68e95497435bef16cf6
Zetao <zetao_wu@brown.edu>
2024-09-07 13:47:28 -0400

    completing sign up api req and initalizing validators

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,9 +1,26 @@
 import User from "../models/User.js";
+import { hash } from 'bcrypt';
 export const getAllUsers = async (req, res, next) => {
     //get all users from db
     try {
         const users = await User.find();
         return res.status(200).json({ message: "Ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error", cause: error });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    //user signup
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({
+            name, email, password: hashedPassword
+        });
+        await user.save();
+        return res.status(201).json({ message: "Ok", id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
+import {hash} from 'bcrypt'
 
 export const getAllUsers = async (
   req: Request,
@@ -16,3 +17,23 @@ export const getAllUsers = async (
 
   }
 };
+
+export const userSignup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    //user signup
+    try {
+    const {name, email, password} = req.body;
+    const hashedPassword = await hash(password, 10);
+      const user = new User({
+        name, email, password: hashedPassword
+      })
+      await user.save();
+      return res.status(201).json({message: "Ok", id:user._id.toString()});
+    } catch (error) {
+      console.log(error)
+      return res.status(200).json({message: "Error", cause: error});
+    }
+  };
