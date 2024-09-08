f73134c869a7326e8f7970164a744cf8a8456d21
Zetao <zetao_wu@brown.edu>
2024-09-08 15:18:05 -0400

    implement the ui for the chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React from "react";
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
+import { red } from "@mui/material/colors";
 
 const Chat = () => {
+  const auth = useAuth();
   return (
-    <div>
-      Chat
-    </div>
-  )
-}
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBOT
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask some questions, advices, or help, but avoid sharing
+            personal information and/or harmful questions.
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: 700,
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red.A400,
+              ":hover": {
+                bgcolor: red[200],
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, sx: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+          }}
+        >
+          Model - GPT 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: 'auto',
+            scrollBehavior: "smooth",
+          }}
+        ></Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default Chat
+export default Chat;
